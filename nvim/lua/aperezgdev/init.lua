87060d083828b7259808b743bdbccd1428619551
Alex Perez Garcia <aperezgdev@gmail.com>
2024-05-03 18:49:21 +0200

    FIRST COMMIT

---
+++ nvim/lua/aperezgdev/init.lua
@@ -0,0 +1,22 @@
+-- Importamos config de plugins
+require("aperezgdev.remap")
+require("aperezgdev.telescope")
+require("aperezgdev.treesitter")
+require("aperezgdev.lsp")
+require("aperezgdev.tokyo-night")
+require("aperezgdev.cmp")
+require("aperezgdev.auto-tag")
+require("aperezgdev.null-ls")
+require("aperezgdev.lualine")
+
+-- Tema establecido
+vim.cmd[[colorscheme tokyonight]]
+
+-- Config para los tabs
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+-- Config para los numeros
+vim.opt.number = true
+vim.opt.relativenumber = true
